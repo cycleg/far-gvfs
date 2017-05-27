cf7bcac8677cf2f0f6a0b9b8b6d0ea77997e6743
Alexei Golovin <golovin.alexei@gmail.com>
2017-05-27 12:28:59 +0300

    SecretServiceStorage added to project.

---
+++ src/SecretServiceStorage.cpp
@@ -0,0 +1,244 @@
+/*
+ * SecretServiceStorage.cpp
+ *
+ *  Created on: 26.05.2017
+ *      Author: cycleg
+ */
+#include <iostream>
+#include <functional>
+#include <libsoup/soup.h> // URI parser
+#include <utils.h> // far2l/utils
+#include "SecretServiceStorage.h"
+
+#define UNUSED(x) (void)x;
+
+using namespace std::placeholders;
+
+namespace {
+
+std::function<void(GObject*, GAsyncResult*, gpointer)>
+  password_stored_callback, password_lookup_callback,
+  password_cleared_callback;
+
+extern "C" void password_stored_wrapper(GObject* source, GAsyncResult* result,
+                                        gpointer user_data)
+{
+  password_stored_callback(source, result, user_data);
+}
+
+extern "C" void password_lookup_wrapper(GObject* source, GAsyncResult* result,
+                                        gpointer user_data)
+{
+  password_lookup_callback(source, result, user_data);
+}
+
+extern "C" void password_cleared_wrapper(GObject* source, GAsyncResult* result,
+                                         gpointer user_data)
+{
+  password_cleared_callback(source, result, user_data);
+}
+
+} // anonymous namespace
+
+SecretServiceStorage::SecretServiceStorage(): m_result(false)
+{
+  password_stored_callback = std::bind(&SecretServiceStorage::onPasswordStored,
+                                       this, _1, _2, _3);
+  password_lookup_callback = std::bind(&SecretServiceStorage::onPasswordFound,
+                                       this, _1, _2, _3);
+  password_cleared_callback = std::bind(&SecretServiceStorage::onPasswordRemoved,
+                                       this, _1, _2, _3);
+}
+
+bool SecretServiceStorage::SavePassword(const std::wstring& url,
+                                       const std::wstring& user,
+                                       const std::wstring& password)
+{
+  std::string uriBuf(StrWide2MB(url)),
+              userBuf(StrWide2MB(user)),
+              passwordBuf(StrWide2MB(password));
+  SoupURI* uri = soup_uri_new(uriBuf.c_str());
+
+  m_password.clear();
+  m_result = false;
+
+  Glib::init();
+  Glib::RefPtr< Glib::MainContext > main_context = Glib::MainContext::create();
+  // Чтобы контекст главного цикла Glib::MainLoop не отслеживал ничего, кроме
+  // операций с ресурсом! Иначе блокируется пользовательский ввод Far.
+  // Из руководства:
+  // This will cause certain asynchronous operations (such as most GIO-based
+  // I/O) which are started in this thread to run under context and deliver
+  // their results to its main loop, rather than running under the global
+  // default context in the main thread.
+  g_main_context_push_thread_default(main_context->gobj());
+  m_mainLoop = Glib::MainLoop::create(main_context, false);
+
+  secret_password_store(SECRET_SCHEMA_COMPAT_NETWORK, // The password type.
+                        SECRET_COLLECTION_DEFAULT, // Where to save it: on disk.
+                        uriBuf.c_str(), // Password label.
+                        passwordBuf.c_str(), // The password itself.
+                        nullptr, // Cancellation object.
+                        password_stored_wrapper, // Callback
+                        nullptr, // User data for callback.
+
+                        // These are the attributes.
+                        "user", userBuf.c_str(), 
+                        "server", soup_uri_get_host(uri),
+                        "protocol", soup_uri_get_scheme(uri),
+
+                        nullptr); // Always end with NULL.
+
+  m_mainLoop->run();
+  // Из руководства:
+  // In some cases however, you may want to schedule a single operation
+  // in a non-default context, or temporarily use a non-default context
+  // in the main thread. In that case, you can wrap the call to the
+  // asynchronous operation inside a
+  // g_main_context_push_thread_default() / g_main_context_pop_thread_default()
+  // pair...
+  // Второй вариант, видимо, наш случай. Без этого вызова Glib выдает assert.
+  g_main_context_pop_thread_default(main_context->gobj());
+  soup_uri_free(uri);
+  return m_result;
+}
+
+bool SecretServiceStorage::LoadPassword(const std::wstring& url,
+                                       const std::wstring& user,
+                                       std::wstring& password)
+{
+  std::string uriBuf(StrWide2MB(url)),
+              userBuf(StrWide2MB(user));
+  SoupURI* uri = soup_uri_new(uriBuf.c_str());
+
+  m_password.clear();
+  m_result = false;
+  password.clear();
+
+  Glib::init();
+  Glib::RefPtr< Glib::MainContext > main_context = Glib::MainContext::create();
+  g_main_context_push_thread_default(main_context->gobj());
+  m_mainLoop = Glib::MainLoop::create(main_context, false);
+
+  secret_password_lookup(SECRET_SCHEMA_COMPAT_NETWORK,
+                         nullptr, // Cancellation object.
+                         password_lookup_wrapper, // Callback
+                         nullptr, // User data for callback.
+
+                         // These are the attributes.
+                         "user", userBuf.c_str(), 
+                         "server", soup_uri_get_host(uri),
+                         "protocol", soup_uri_get_scheme(uri),
+
+                         nullptr); // Always end with NULL.
+  m_mainLoop->run();
+  g_main_context_pop_thread_default(main_context->gobj());
+  soup_uri_free(uri);
+  if (m_result)
+  {
+    StrMB2Wide(m_password, password);
+  }
+  return m_result;
+}
+
+void SecretServiceStorage::RemovePassword(const std::wstring& url,
+                                         const std::wstring& user)
+{
+  std::string uriBuf(StrWide2MB(url)),
+              userBuf(StrWide2MB(user));
+  SoupURI* uri = soup_uri_new(uriBuf.c_str());
+
+  m_password.clear();
+  m_result = false;
+
+  Glib::init();
+  Glib::RefPtr< Glib::MainContext > main_context = Glib::MainContext::create();
+  g_main_context_push_thread_default(main_context->gobj());
+  m_mainLoop = Glib::MainLoop::create(main_context, false);
+
+  secret_password_clear(SECRET_SCHEMA_COMPAT_NETWORK,
+                        nullptr, // Cancellation object.
+                        password_cleared_wrapper, // Callback
+                        nullptr, // User data for callback.
+
+                        // These are the attributes.
+                        "user", userBuf.c_str(), 
+                        "server", soup_uri_get_host(uri),
+                        "protocol", soup_uri_get_scheme(uri),
+
+                        nullptr); // Always end with NULL.
+  m_mainLoop->run();
+  g_main_context_pop_thread_default(main_context->gobj());
+  soup_uri_free(uri);
+}
+
+void SecretServiceStorage::onPasswordStored(GObject* source,
+                                           GAsyncResult* result,
+                                           gpointer user_data)
+{
+  UNUSED(source);
+  UNUSED(user_data);
+
+  GError* error = nullptr;
+  secret_password_store_finish(result, &error);
+  m_result = (error == nullptr);
+  if (!m_result)
+  {
+    std::cerr << "SecretServiceStorage couldn't save password: "
+              << error->message << std::endl;
+    g_error_free (error);
+  }
+  m_mainLoop->quit();
+}
+
+void SecretServiceStorage::onPasswordFound(GObject* source,
+                                          GAsyncResult* result,
+                                          gpointer user_data)
+{
+  UNUSED(source);
+  UNUSED(user_data);
+
+  GError* error = nullptr;
+  gchar* password = secret_password_lookup_finish(result, &error);
+  m_result = (error == nullptr);
+  if (m_result)
+    {
+      if (password != nullptr)
+        {
+          m_password = password;
+          secret_password_free(password);
+        }
+        else
+        {
+          std::cerr << "SecretServiceStorage couldn't find password" << std::endl;
+          m_result = false;
+        }
+    }
+    else
+    {
+      std::cerr << "SecretServiceStorage couldn't find password: "
+                << error->message << std::endl;
+      g_error_free (error);
+    }
+  m_mainLoop->quit();
+}
+
+void SecretServiceStorage::onPasswordRemoved(GObject* source,
+                                            GAsyncResult* result,
+                                            gpointer user_data)
+{
+  UNUSED(source);
+  UNUSED(user_data);
+
+  GError* error = NULL;
+  secret_password_clear_finish(result, &error);
+  // отсутствие пароля -- не ошибка
+  m_result = (error == nullptr);
+  if (!m_result)
+  {
+    std::cerr << "SecretServiceStorage couldn't remove password: "
+              << error->message << std::endl;
+    g_error_free (error);
+  }
+  m_mainLoop->quit();
+}
---
+++ src/SecretServiceStorage.h
@@ -0,0 +1,111 @@
+/*
+ * SecretServiceStorage.h
+ *
+ *  Created on: 26.05.2017
+ *      Author: cycleg
+ */
+#pragma once
+
+#include <string>
+#include <libsecret/secret.h>
+#include <gtkmm.h>
+
+/// 
+/// @brief Хранилище паролей в системных безопасных хранилищах.
+
+/// Класс представляет собой обертку над API libsecret. Поддерживаются три
+/// операции:
+///
+/// * сохранить пароль;
+/// * извлечь пароль;
+/// * удалить пароль.
+///
+/// В зависимости от используемого окружения рабочего стола пароли хранятся в
+/// Gnome Keyring или KDE Wallet.
+///
+/// Пароли помещаются в коллекцию по умолчанию (SECRET_COLLECTION_DEFAULT) по
+/// схеме сетевого пароля (SECRET_SCHEMA_COMPAT_NETWORK). Ключом для поиска
+/// пароля в коллекции выступают имя пользователя для аутентификации на
+/// ресурсе, сервер и протокол из URL ресурса.
+///
+/// Хотя внутри операции выполняются асинхронно, интерфейс самого класса --
+/// синхронный. После завершения одной из трех вышеуказанных операций в этом
+/// же экземпляре может быть запущена другая. Однако класс -- не
+/// реентерабельный!
+///
+/// @author cycleg
+///
+class SecretServiceStorage
+{
+  public:
+    ///
+    /// Конструктор.
+    ///
+    SecretServiceStorage();
+
+    ///
+    /// Сохранить пароль для данного URL в связке.
+    ///
+    /// @param [in] url URL ресурса.
+    /// @param [in] user Имя пользователя.
+    /// @param [in] password Сохраняемый пароль.
+    /// @return Результат операции.
+    ///
+    /// Возвращает false, если сохранить не удалось.
+    ///
+    bool SavePassword(const std::wstring& url, const std::wstring& user,
+                      const std::wstring& password);
+    ///
+    /// Найти пароль для данного URL в связке.
+    ///
+    /// @param [in] url URL ресурса.
+    /// @param [in] user Имя пользователя.
+    /// @param [out] password Найденный пароль.
+    /// @return Результат операции.
+    ///
+    /// Возвращает false, если пароль не найден, в password -- пустая строка.
+    ///
+    bool LoadPassword(const std::wstring& url, const std::wstring& user,
+                      std::wstring& password);
+    ///
+    /// Удалить из связки пароль для данного URL.
+    ///
+    /// @param [in] url URL ресурса.
+    /// @param [in] user Имя пользователя.
+    ///
+    void RemovePassword(const std::wstring& url, const std::wstring& user);
+
+  private:
+    ///
+    /// Обратный вызов после сохранения пароля.
+    ///
+    /// @param [in] source Объект, инициировавший асинхронную операцию.
+    /// @param [in] result Результат операции.
+    /// @param [in] user_data В текущей реализации всегда nullptr.
+    ///
+    void onPasswordStored(GObject* source, GAsyncResult* result,
+                          gpointer user_data);
+    ///
+    /// Обратный вызов после завершения поиска пароля.
+    ///
+    /// @param [in] source Объект, инициировавший асинхронную операцию.
+    /// @param [in] result Результат операции.
+    /// @param [in] user_data В текущей реализации всегда nullptr.
+    ///
+    void onPasswordFound(GObject* source, GAsyncResult* result,
+                         gpointer user_data);
+    ///
+    /// Обратный вызов после удаления пароля.
+    ///
+    /// @param [in] source Объект, инициировавший асинхронную операцию.
+    /// @param [in] result Результат операции.
+    /// @param [in] user_data В текущей реализации всегда nullptr.
+    ///
+    void onPasswordRemoved(GObject* source, GAsyncResult* result,
+                           gpointer user_data);
+
+    bool m_result; ///< Результат последней асинхронной операции.
+    std::string m_password; ///< Буфер для найденного пароля. Используется
+                            ///< в LoadPassword().
+    Glib::RefPtr<Glib::MainLoop> m_mainLoop; ///< Главный цикл glib.
+};
